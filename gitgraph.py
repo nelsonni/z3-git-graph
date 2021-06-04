b48c0d20a8be5a6b4f7a23881601b67eb6951cb9
nelsonni <nelsonni@oregonstate.edu>
2021-06-04 12:21:42 -0700

    Minor naming cleanup and expanded printing fn

--- gitgraph.py
+++ gitgraph.py
@@ -1,5 +1,4 @@
 import argparse
-from unicodedata import numeric
 from tqdm import tqdm # for progressbar
 from git import Repo
 from git.objects.commit import Commit
@@ -12,7 +11,7 @@ class CommitType (Enum):
     SEQUENTIAL = 2
     STRUCTURAL = 3
 
-class Vector():
+class Vertex():
 
     commit: Commit = {}
     parents: List[str] = []
@@ -20,7 +19,7 @@ class Vector():
     processed = False
     branching = False
     merging = False
-    types: CommitType
+    commitType: CommitType
 
     def __init__(self, commit: Commit):
         self.commit = commit
@@ -28,12 +27,12 @@ class Vector():
         self.children = []
 
     def __repr__(self):
-        return 'Vector {0}: commit: {{ hexsha: {1}, parents: {2} }}, parents: {3}, children: {4}, processed: {5}'.format(
+        return 'Vertex {0}: commit: {{ hexsha: {1}, parents: {2} }}, parents: {3}, children: {4}, processed: {5}'.format(
             self.commit.hexsha, self.commit.hexsha, self.commit.parents, 
             self.parents, self.children, self.processed)
     
     def __str__(self):
-        return 'Vector {0}: commit: {{ hexsha: {1}, parents: {2} }}, parents: {3}, children: {4}, processed: {5}'.format(
+        return 'Vertex {0}: commit: {{ hexsha: {1}, parents: {2} }}, parents: {3}, children: {4}, processed: {5}'.format(
             self.commit.hexsha, self.commit.hexsha, self.commit.parents, 
             self.parents, self.children, self.processed)
 
@@ -48,7 +47,7 @@ class Vector():
 class GitGraph():
 
     repo = {}
-    vectors: Dict[str, Vector] = dict()                     # all commits
+    vertices: Dict[str, Vertex] = dict()                    # all commits
     edges: List[Tuple[str, str]] = []                       # all edges
 
     def __init__(self, root: PathLike):
@@ -61,23 +60,23 @@ class GitGraph():
             self.edges.append(candidate)
 
     def __addVector__(self, commit: Commit) -> bool:
-        if self.vectors.get(commit.hexsha) is None:         # no duplicate commits
-            self.vectors.update({commit.hexsha: Vector(commit)})
+        if self.vertices.get(commit.hexsha) is None:        # no duplicate commits
+            self.vertices.update({commit.hexsha: Vertex(commit)})
 
     def __addParent__(self, parent: str, child: Commit):
-        self.__addVector__(self.repo.commit(parent)) # guarantees parent vector exists
-        self.vectors.get(parent.hexsha).children.append(child.hexsha)
+        self.__addVector__(self.repo.commit(parent))        # guarantee parent vector exists
+        self.vertices.get(parent.hexsha).children.append(child.hexsha)
         self.__addEdge__(child.hexsha, parent.hexsha)
     
     def __parseParents__(self, child: Commit):
-        if self.vectors.get(child.hexsha).processed is False:
-            self.vectors.get(child.hexsha).processed = True
+        if self.vertices.get(child.hexsha).processed is False:
+            self.vertices.get(child.hexsha).processed = True
             for parent in child.parents:
-                self.vectors.get(child.hexsha).parents.append(parent.hexsha)
+                self.vertices.get(child.hexsha).parents.append(parent.hexsha)
                 self.__addParent__(parent, child)
 
-    def getData(self) -> Dict[str, Union[List[Vector], List[Tuple[str, str]]]]:
-        return { "vectors": list(self.vectors.values()), "edges": List[Tuple[str, str]] }
+    def getData(self) -> Dict[str, Union[List[Vertex], List[Tuple[str, str]]]]:
+        return { "vertices": list(self.vertices.values()), "edges": self.edges }
 
     def parse(self):
         branches = filter(lambda ref: ref not in self.repo.tags, self.repo.refs)
@@ -89,38 +88,38 @@ class GitGraph():
                 self.__parseParents__(commit)               # only add none duplicate parent vectors and edges
     
     def assign(self):
-        terminals = list(filter(lambda vector: len(vector.parents) == 0 or len(vector.children) == 0, self.vectors.values()))
-        sequentials = list(filter(lambda vector: len(vector.parents) == 1 and len(vector.children) == 1, self.vectors.values()))
-        structurals = list(filter(lambda vector: (vector not in terminals) and (len(vector.parents) + len(vector.children)) > 2, self.vectors.values()))
-        branching = list (filter (lambda vector: (len(vector.children) > 1) and (len(vector.parents) >= 1), self.vectors.values()))
-        merging = list (filter (lambda vector: (len(vector.children) >= 1) and (len(vector.parents) > 1), self.vectors.values()))
+        terminals = list(filter(lambda vector: len(vector.parents) == 0 or len(vector.children) == 0, self.vertices.values()))
+        sequentials = list(filter(lambda vector: len(vector.parents) == 1 and len(vector.children) == 1, self.vertices.values()))
+        structurals = list(filter(lambda vector: (vector not in terminals) and (len(vector.parents) + len(vector.children)) > 2, self.vertices.values()))
+        branching = list (filter (lambda vector: (len(vector.children) > 1) and (len(vector.parents) >= 1), self.vertices.values()))
+        merging = list (filter (lambda vector: (len(vector.children) >= 1) and (len(vector.parents) > 1), self.vertices.values()))
 
-        vectors = list (self.vectors.values())
+        vectors = list (self.vertices.values())
         for vector in vectors:
             if (vector in terminals):
-                vector.types = CommitType.TERMINAL
+                vector.commitType = CommitType.TERMINAL
             if (vector in sequentials):
-                vector.types = CommitType.SEQUENTIAL
+                vector.commitType = CommitType.SEQUENTIAL
             if (vector in structurals):
-                vector.types = CommitType.STRUCTURAL
+                vector.commitType = CommitType.STRUCTURAL
             if (vector in branching):
                 vector.branching = True
             if (vector in merging):
                 vector.merging = True
 
     def prune(self):
-        sequentials = tqdm(list(filter(lambda vector: len(vector.parents) == 1 and len(vector.children) == 1, self.vectors.values())))
+        sequentials = tqdm(list(filter(lambda vertex: len(vertex.parents) == 1 and len(vertex.children) == 1, self.vertices.values())))
         for sequential in sequentials:
             sequentials.set_description('{0}'.format(sequential.commit.hexsha))
 
             # handle the parent vector
-            parent = self.vectors[sequential.parents[0]]
+            parent = self.vertices[sequential.parents[0]]
             parentEdges = list(filter(lambda edge: [sequential.commit.hexsha, sequential.parents[0]] == edge, self.edges))
             for edge in parentEdges:
                 self.edges.remove(edge)
 
             # handle the child vector
-            child = self.vectors.get(sequential.children[0])
+            child = self.vertices.get(sequential.children[0])
             childEdges = list(filter(lambda edge: [sequential.children[0], sequential.commit.hexsha] == edge, self.edges))
             for edge in childEdges:
                 self.edges.remove(edge)
@@ -130,16 +129,19 @@ class GitGraph():
                 self.__addEdge__(child.commit.hexsha, parent.commit.hexsha)
 
             # remove the sequential vector
-            self.vectors.pop(sequential.commit.hexsha)
+            self.vertices.pop(sequential.commit.hexsha)
 
-    def print(self):
-        print('Vectors: {0}, Edges: {1}'.format(len(self.vectors), len(self.edges)))
-        terminals = list(filter(lambda vector: len(vector.parents) == 0 or len(vector.children) == 0, self.vectors.values()))
-        sequentials = list(filter(lambda vector: len(vector.parents) == 1 and len(vector.children) == 1, self.vectors.values()))
-        structurals = list(filter(lambda vector: (vector not in terminals) and (len(vector.parents) + len(vector.children)) > 2, self.vectors.values()))
-        print('  Terminals: {0}'.format(len(terminals)))
-        print('  Sequentials: {0}'.format(len(sequentials)))
-        print('  Structurals: {0}'.format(len(structurals)))
+    def print(self, show_all = False):
+        data = self.getData()
+        print('Vectors: {0}, Edges: {1}'.format(len(data['vertices']), len(data['edges'])))
+        print('  Terminals: {0}'.format(len(list(filter(lambda vertex: vertex.commitType == CommitType.TERMINAL, data['vertices'])))))
+        print('  Sequentials: {0}'.format(len(list(filter(lambda vertex: vertex.commitType == CommitType.SEQUENTIAL, data['vertices'])))))
+        print('  Structurals: {0}'.format(len(list(filter(lambda vertex: vertex.commitType == CommitType.STRUCTURAL, data['vertices'])))))
+        print('  Branching: {0}'.format(len(list(filter(lambda vertex: vertex.branching == True, data['vertices'])))))
+        print('  Merging: {0}'.format(len(list(filter(lambda vertex: vertex.merging == True, data['vertices'])))))
+        if show_all:
+            for key, value in data.items():
+                print(key, ' : ', value)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("root")
@@ -149,3 +151,4 @@ graph = GitGraph(args.root)
 graph.parse()
 graph.prune()
 graph.assign()
+graph.print()
