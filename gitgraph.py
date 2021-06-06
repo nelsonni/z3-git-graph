f97565403671964628791b22778a69812e5e007c
nelsonni <nelsonni@oregonstate.edu>
2021-06-05 17:39:51 -0700

    partial

--- gitgraph.py
+++ gitgraph.py
@@ -11,26 +11,23 @@ class Vertex():
     commit: Commit = {}
     parents: List[str] = []
     children: List[str] = []
-    processed = False
     branching = False
     merging = False
     terminal = False
     sequential = False
     structural = False
 
-    def __init__(self, branch: str, commit: Commit):
-        self.branch = branch
+    def __init__(self, commit: Commit, branch: str):
         self.commit = commit
-        self.parents = []
+        self.branch = branch
+        self.parents = [parent.hexsha for parent in commit.parents]
         self.children = []
 
     def __hash__(self):
         return self.commit.hexsha
 
     def __repr__(self):
-        return 'Vertex {0}: commit: {{ hexsha: {1}, parents: {2} }}, parents: {3}, children: {4}, processed: {5}, branching: {6}, merging: {7}'.format(
-            self.commit.hexsha, self.commit.hexsha, self.commit.parents, self.parents, self.children, 
-            self.processed, self.branching, self.merging)
+        return '{0}\n\tparents: {1}\n\tchildren: {2}\n\tterminal: {3}\n\tsequential: {4}\n\tstructural: {5}\n'.format(self.commit.hexsha, self.parents, self.children, self.terminal, self.sequential, self.structural)
     
     def __str__(self):
         return self.__repr__()
@@ -47,12 +44,14 @@ class Vertex():
 class GitGraph():
 
     repo = {}
-    branches: Dict[str, List[Vertex]] = dict()
-    vertices: Dict[str, Vertex] = dict()                    # all vertices; key is hexsha, value is commit
+    branches: List[str] = []                                # all branches; 'alias/name' format (e.g. 'origin/main')
+    vertices: List[Vertex] = []                             # all vertices; [hexsha: Vertex] format
     edges: List[Tuple[Vertex, Vertex]] = []                 # all directed edges; [start, end] format
 
     def __init__(self, root: PathLike):
         self.repo = Repo(root)
+        self.branches = list(filter(lambda ref: ref not in self.repo.tags, self.repo.refs))
+        self.parse()
 
     def __addEdge__(self, start: Vertex, end: Vertex):
         candidate = [start, end]
@@ -60,61 +59,54 @@ class GitGraph():
         if len(existing) == 0:                              # no duplicate directed edges
             self.edges.append(candidate)
 
-    def __addVertex__(self, branch: str, commit: Commit) -> bool:
-        if self.vertices.get(commit.hexsha) is None:        # no duplicate commits
-            newVertex = Vertex(branch, commit)
-            branchVertices = self.branches.get(branch)
-            if branchVertices:
-                self.branches.update({branch: branchVertices.append(newVertex)})
-            self.vertices.update({commit.hexsha: newVertex})
+    def __addVertex__(self, commit: Commit, branch: str):
+        # print("addVertex: {0}".format(commit.hexsha))
+        exists = len([v for v in self.vertices if v.commit.hexsha == commit.hexsha]) > 0
+        if not exists:                                      # no duplicate commits
+            self.vertices.append(Vertex(commit, branch))
 
-    def __addParent__(self, branch: str, parent: str, child: Commit):
-        self.__addVertex__(branch, self.repo.commit(parent))        # guarantee parent vector exists
-        self.vertices.get(parent.hexsha).children.append(child.hexsha)
-        self.__addEdge__(self.vertices[child.hexsha], self.vertices[parent.hexsha])
-    
-    def __parseParents__(self, branch: str, child: Commit):
-        if self.vertices.get(child.hexsha).processed is False:
-            self.vertices.get(child.hexsha).processed = True
-            for parent in child.parents:
-                self.vertices.get(child.hexsha).parents.append(parent.hexsha)
-                self.__addParent__(branch, parent, child)
+    def __getVertex__(self, hexsha: str):
+        search = [v for v in self.vertices if v.commit.hexsha == hexsha]
+        return search[0] if len(search) > 0 else None
 
-    def getData(self):
-        self.parse()
-        self.prune()
-        self.assign()
-        return GitGraphData(self)
+    def __updateVertex__(self, updated: Vertex):
+        [updated if updated.commit.hexsha == vertex.commit.hexsha else vertex for vertex in self.vertices]
+
+    def __categorize__(self, vertex: Vertex):
+        if (len(vertex.parents) == 0 or len(vertex.children) == 0):
+            vertex.terminal = True
+        if (len(vertex.parents) == 1 and len(vertex.children) == 1):
+            vertex.sequential = True
+        if (len(vertex.parents) + len(vertex.children) > 2):
+            vertex.structural = True
+        if (len(vertex.children) > 1 and len(vertex.parents) > 0):
+            vertex.branching = True
+        if (len(vertex.children) > 0 and len(vertex.parents) > 1):
+            vertex.merging = True
+        self.__updateVertex__(vertex)
 
     def parse(self):
-        branches = filter(lambda ref: ref not in self.repo.tags, self.repo.refs)
-        progress = tqdm(list(branches))
+        print("Parsing git for branches and commits...")
+        progress = tqdm(self.branches)
         for branch in progress:
             progress.set_description('{0}'.format(branch))
             for commit in self.repo.iter_commits(branch):
-                self.__addVertex__(branch, commit)          # only add none duplicate vectors
-                self.__parseParents__(branch, commit)       # only add none duplicate parent vectors and edges
+                self.__addVertex__(commit, branch)          # only add none duplicate vectors
+
+        print("Parsing vertices for parent and child linkage...")
+        progress = tqdm(self.vertices)
+        for vertex in progress:
+            progress.set_description('{0}'.format(vertex.commit.hexsha))
+            for hexsha in vertex.parents:
+                parent = self.__getVertex__(hexsha)
+                if parent:
+                    self.__addEdge__(vertex, parent)
+                    parent.children.append(vertex.commit.hexsha)
+                    self.__updateVertex__(parent)
+            self.__categorize__(vertex)
     
-    def assign(self):
-        terminals = list(filter(lambda vector: len(vector.parents) == 0 or len(vector.children) == 0, self.vertices.values()))
-        sequentials = list(filter(lambda vector: len(vector.parents) == 1 and len(vector.children) == 1, self.vertices.values()))
-        structurals = list(filter(lambda vector: (vector not in terminals) and (len(vector.parents) + len(vector.children)) > 2, self.vertices.values()))
-        branching = list (filter (lambda vector: (len(vector.children) > 1) and (len(vector.parents) >= 1), self.vertices.values()))
-        merging = list (filter (lambda vector: (len(vector.children) >= 1) and (len(vector.parents) > 1), self.vertices.values()))
-
-        for vertex in list(self.vertices.values()):
-            if (vertex in terminals):
-                vertex.terminal = True
-            if (vertex in sequentials):
-                vertex.sequential = True
-            if (vertex in structurals):
-                vertex.structural = True
-            if (vertex in branching):
-                vertex.branching = True
-            if (vertex in merging):
-                vertex.merging = True
-
     def prune(self):
+        print("Pruning sequential vertices from the graph...")
         sequentials = tqdm(list(filter(lambda vertex: len(vertex.parents) == 1 and len(vertex.children) == 1, self.vertices.values())))
         for sequential in sequentials:
             sequentials.set_description('{0}'.format(sequential.commit.hexsha))
@@ -142,28 +134,33 @@ class GitGraph():
             self.vertices.pop(sequential.commit.hexsha)
 
     def print(self, show_all = False):
-        data = self.getData()
-        print('Vectors: {0}, Edges: {1}'.format(len(data.vertices), len(data.edges)))
-        print('  Terminals: {0}'.format(len(list(filter(lambda vertex: vertex.terminal == True, data.vertices)))))
-        print('  Sequentials: {0}'.format(len(list(filter(lambda vertex: vertex.sequential == True, data.vertices)))))
-        print('  Structurals: {0}'.format(len(list(filter(lambda vertex: vertex.structural == True, data.vertices)))))
-        print('  Branching: {0}'.format(len(list(filter(lambda vertex: vertex.branching == True, data.vertices)))))
-        print('  Merging: {0}'.format(len(list(filter(lambda vertex: vertex.merging == True, data.vertices)))))
+        print('Vectors: {0}, Edges: {1}'.format(len(self.vertices), len(self.edges)))
+        print('  Terminals: {0}'.format(len(list(filter(lambda vertex: vertex.terminal == True, self.vertices)))))
+        print('  Sequentials: {0}'.format(len(list(filter(lambda vertex: vertex.sequential == True, self.vertices)))))
+        print('  Structurals: {0}'.format(len(list(filter(lambda vertex: vertex.structural == True, self.vertices)))))
+        print('  Branching: {0}'.format(len(list(filter(lambda vertex: vertex.branching == True, self.vertices)))))
+        print('  Merging: {0}'.format(len(list(filter(lambda vertex: vertex.merging == True, self.vertices)))))
         if show_all:
-            for key, value in data.items():
-                print(key, ' : ', value)
+            print("VERTICES:")
+            for v in self.vertices:
+                print(v)
+            print("EDGES:")
+            for e in self.edges:
+                print(e)
+            # for key, value in self.items():
+            #     print(key, ' : ', value)
 
 
 class GitGraphIterator:
 
-    def __init__(self, graph):
+    def __init__(self, graph: GitGraph):
         self._graph = graph
         self._branch_index = 0
         self._vertex_index = 0
 
     def __next__(self):
         if self._branch_index < len(self._graph.branches):
-            branchVertices = self._graph.branches.keys()[self._branch_index]
+            branchVertices = [v for v in self._graph.vertices if v.branch == self._graph.branches[self._branch_index]]
             if self._vertex_index == len(branchVertices):
                 self._branch_index += 1
                 self._vertex_index = 0
@@ -173,6 +170,7 @@ class GitGraphIterator:
                 self._vertex_index += 1
                 return result
         raise StopIteration
+
 class GitGraphData():
 
     branches: Dict[str, List[Vertex]] = dict()
@@ -186,6 +184,9 @@ class GitGraphData():
 
     def __iter__(self):
         return GitGraphIterator(self)
+
+    def print(self):
+        print(self.vertices)
 
 # parser = argparse.ArgumentParser()
 # parser.add_argument("root")
--- sat_solver.py
+++ sat_solver.py
@@ -1,53 +1,91 @@
 import argparse
-from pysmt.shortcuts import Equals, Symbol, And, Not, GE, Int, get_model
+from pysmt.shortcuts import Symbol, Or, And, GE, LT, Plus, Equals, Int, get_model
 from pysmt.typing import INT
-from z3 import *
-from gitgraph import GitGraph, Vertex
+from gitgraph import GitGraph
 
 parser = argparse.ArgumentParser()
 parser.add_argument("root")
 args = parser.parse_args()
 
-graph = GitGraph(args.root).getData()
+graph = GitGraph(args.root)
+graph.print()
+domains = And([ Or(v.terminal, v.sequential) for v in graph.vertices ])
+# result = [graph.vertices[i:i+2] for i in range(len(graph.vertices)-2+1)]
+# print(result)
+# problem = And([And(v.structural, u.structural) for v,u in   ])
 
-def isTerminal(v: Vertex):
-	return Bool(v.terminal)
 
-def isStructural(v: Vertex):
-	return Equals(v.structural, True)
+# hello = [Symbol(s, INT) for s in "hello"]
+# world = [Symbol(s, INT) for s in "world"]
+# letters = set(hello+world)
+# domains = And([And(GE(l, Int(1)),
+#                    LT(l, Int(10))) for l in letters])
 
-def isBranching(v: Vertex):
-	return Equals(v.branching, True)
+# sum_hello = Plus(hello) # n-ary operators can take lists
+# sum_world = Plus(world) # as arguments
+# problem = And(Equals(sum_hello, sum_world),
+#               Equals(sum_hello, Int(25)))
+# formula = And(domains, problem)
 
-def isMerging(v: Vertex):
-	return Equals(v.merging, True)
+# print("Serialization of the formula:")
+# print(formula)
 
-def uniqCommitType(v: Vertex):
-	return Or( And(isTerminal(v), Not(isStructural(v))), And(Not(isTerminal(v)), isStructural(v)) )
+# model = get_model(formula)
+# if model:
+#   print(model)
+# else:
+#   print("No solution found")
 
-validTypes = And([ isTerminal(v) for v in graph.vertices ])
+# import argparse
+# from pysmt.shortcuts import Equals, Symbol, And, Not, GE, Int, get_model
+# from pysmt.typing import INT
+# from z3 import *
+# from gitgraph import GitGraph, Vertex
 
-def branchThenMerge(u: Vertex, v: Vertex):
-	return Implies(isBranching(u), isMerging(v))
+# parser = argparse.ArgumentParser()
+# parser.add_argument("root")
+# args = parser.parse_args()
 
-def multipleEdges(u: Vertex, v: Vertex):
-	count = Symbol(len([Equals(child, v.commit.hexsha) for child in u.children]), INT)
-	return GE(count, Int(2))
+# graph = GitGraph(args.root).getData()
 
-validOrdering = Or([ branchThenMerge(u,v) for u,v in graph ])
+# def isTerminal(v: Vertex):
+# 	return Bool(v.terminal)
 
-formula = validTypes
+# def isStructural(v: Vertex):
+# 	return Equals(v.structural, True)
 
-# # Solve:
-model = get_model(formula)
-if model:
-	print(model)
-	print(eval(formula))
-else:
-	print("No solution found")
+# def isBranching(v: Vertex):
+# 	return Equals(v.branching, True)
 
-# if r == sat:
-#     m = s.model()
-#     for d in m:
-#         print(d, m[d])
+# def isMerging(v: Vertex):
+# 	return Equals(v.merging, True)
+
+# def uniqCommitType(v: Vertex):
+# 	return Or( And(isTerminal(v), Not(isStructural(v))), And(Not(isTerminal(v)), isStructural(v)) )
+
+# validTypes = And([ isTerminal(v) for v in graph.vertices ])
+
+# def branchThenMerge(u: Vertex, v: Vertex):
+# 	return Implies(isBranching(u), isMerging(v))
+
+# def multipleEdges(u: Vertex, v: Vertex):
+# 	count = Symbol(len([Equals(child, v.commit.hexsha) for child in u.children]), INT)
+# 	return GE(count, Int(2))
+
+# validOrdering = Or([ branchThenMerge(u,v) for u,v in graph ])
+
+# formula = validTypes
+
+# # # Solve:
+# model = get_model(formula)
+# if model:
+# 	print(model)
+# 	print(eval(formula))
+# else:
+# 	print("No solution found")
+
+# # if r == sat:
+# #     m = s.model()
+# #     for d in m:
+# #         print(d, m[d])
 
